488d61866c09e8f50f0e04904feb7de24c6af20b
lehenshtein <lehenshtein@gmail.com>
2022-07-05 02:26:27 +0300

    configs, material, akita, service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AkitaNgDevtools} from '@datorama/akita-ngdevtools';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ReactiveFormsModule} from '@angular/forms';
+import {LayoutModule} from '@app/layout/layout.module';
+import {environment} from '@environment/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    environment.production ? [] : AkitaNgDevtools.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+      // registrationStrategy: 'registerImmediately'
+    }),
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatSliderModule} from '@angular/material/slider';
+
+const material = [
+  MatSliderModule
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ...material
+  ],
+  exports: [
+    ...material
+  ]
+})
+export class SharedModule { }

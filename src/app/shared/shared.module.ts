e0fede30e0e7c0009b532e68c29a8994ee9029e0
lehenshtein <lehenshtein@gmail.com>
2022-07-05 22:35:46 +0300

    custom material palette header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatSliderModule} from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 const material = [
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
   MatSliderModule
 ]
 

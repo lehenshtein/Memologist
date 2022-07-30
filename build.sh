b45dd2f35b416353d677db5f3234ba3413fca1a5
lehenshtein <lehenshtein@gmail.com>
2022-07-30 10:42:32 +0300

    environments change

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 
 echo "Building Angular app for $NODE_ENV"
 
-build_stage='ng build --configuration=staging && ng run memologist:server'
+build_stage='ng build --configuration=staging && ng run memologist:server:staging'
 if [ $NODE_ENV = "staging" ]; then
  echo "running $build_stage ..."
  eval "$build_stage"
 fi
 
-build_prod='ng build && ng run memologist:server'
+build_prod='ng build && ng run memologist:server:production'
 if [ $NODE_ENV = "production" ]; then
  echo "running $build_prod ..."
  eval "$build_prod"
